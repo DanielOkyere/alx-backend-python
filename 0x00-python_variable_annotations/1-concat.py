f1b71fba947e47a6e789bfff983d92d7135e883b
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-02-02 15:14:18 +0000

    0-2 done

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - concat
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concat
+         Takes two strings as argument
+    Args
+       str1: first string 
+       str2: second string
+    Return:
+        Return a concatenation of the two strings
+    """
+    return "{}{}".format(str1, str2)
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Typed annotattion for floor function
+"""
+import math
+
+def floor(n: float) -> int:
+    """
+    Floor
+        Takes a float value and returns a floor
+        float of the result
+    """
+    return math.floor(n)
