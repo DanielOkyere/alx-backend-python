aa26e1bb13bf202ac98175e3ff652b728a3c2493
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-02-02 15:46:31 +0000

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Complex types - list of floats
+"""
+from typing import List
+
+
+def sum_list(input_list: [List[float]]) -> float:
+    """
+    Sums a list of floats
+    """
+    if input_list is None:
+        return 0
+    else:
+        return sum(input_list)
