b9e597af4047b0f34fa799774821a13df039219f
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-02-02 15:49:09 +0000

    modified 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -8,6 +8,10 @@ from typing import List
 def sum_list(input_list: [List[float]]) -> float:
     """
     Sums a list of floats
+    Args:
+        input_list (list): A list of floats
+    Returns:
+        float: The sum of the floats in the list
     """
     if input_list is None:
         return 0
