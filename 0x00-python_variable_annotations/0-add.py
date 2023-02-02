19773070de1a5ae09d1892c0226820ca4c4d3af4
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-02-02 10:57:01 +0000

    removed all __pycache__ files

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Type  annotated function add that
+takes two floats as arguments
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add function
+    Returns
+       Float value
+    """
+    return a + b
