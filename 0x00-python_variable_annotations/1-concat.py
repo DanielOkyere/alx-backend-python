44651807c225ac99a5e8f29ab45b2337fed0fc27
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-02-02 15:16:34 +0000

    1-concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,12 +3,13 @@
 Basic annotations - concat
 """
 
+
 def concat(str1: str, str2: str) -> str:
     """
     Concat
          Takes two strings as argument
     Args
-       str1: first string 
+       str1: first string
        str2: second string
     Return:
         Return a concatenation of the two strings
