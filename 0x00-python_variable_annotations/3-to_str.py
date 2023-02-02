afe482128e192454d8da779b4b1f6f85edba0179
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-02-02 15:24:20 +0000

    3-to_str.py done

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    to_str
+        Takes a float value and returns a string of the float number
+    """
+    return "{}".format(n)
