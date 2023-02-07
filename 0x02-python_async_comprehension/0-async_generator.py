91796244538ff078d2916cd6d90f32ca4853332d
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-02-07 20:03:55 +0000

    all done

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,10 +8,10 @@ import asyncio
 from typing import Generator
 
 
-def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Loops 10 times and yields a random number between 0 and 10
     """
-    for in range(10):
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,7 +3,7 @@
 Returns 10 random numbers using async comprehension
 """
 from typing import List
-async_generator = __import__('0.async_generator').async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
