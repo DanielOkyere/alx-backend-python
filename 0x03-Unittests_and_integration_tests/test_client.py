b9200566176b1b418638cb9250362c21a18ab3e5
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-02-27 09:02:56 +0000

    updates on test_client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Test Client Document """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 
 import client
@@ -20,3 +20,13 @@ class TestGithubOrgClient(unittest.TestCase):
         t_response = org_test.org
         self.assertEqual(t_response, mock_org.return_value)
         mock_org.asser_called_once()
+
+    def test_public_repos_url(self):
+        """Test TestGitHubOrgClients method"""
+        with patch.object(GithubOrgClient,
+                          'org', new_callable=PropertyMock) as m:
+            m.return_value = {"repos_url": "89"}
+            test_org = GithubOrgClient('holberton')
+            t_rep_url = test_org._public_repos_url
+            self.assertEqual(t_rep_url, m.return_value.get('repos_url'))
+            m.assert_called_once()
